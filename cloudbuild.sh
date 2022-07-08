cefbd73b15a8771896129ebff389dcb0c47405ff
Brandon Fan <brandonfan1256@gmail.com>
2022-07-08 15:23:46 -0400

    update cloud build with convnext

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -1,14 +1,17 @@
 declare -a ROS_VERSIONS=( "foxy" "galactic" "humble" "rolling" )
 
 ORGANIZATION="facebook"
-declare -a MODEL_VERSIONS=( "convnext-base-224" "convnext-tiny-224" "convnext-small-224" "convnext-large-224" "convnext-base-384" "convnext-large-384" )
+MODEL_NAME="convnext"
+declare -a MODEL_VERSIONS=( "base-224" "tiny-224" "small-224" "large-224" "base-384" "large-384" )
 
 for VERSION in "${ROS_VERSIONS[@]}"
 do
   for MODEL_VERSION in "${MODEL_VERSIONS[@]}"
   do
     ROS_VERSION="$VERSION"
-    gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_MODEL_VERSION="$MODEL_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
+    HF_VERSION="$MODEL_NAME-$MODEL_VERSION"
+    TAG="$MODEL_VERSION"
+    gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_VERSION="$ROS_VERSION",_TAG="$TAG",_MODEL_VERSION="$HF_VERSION",_ORGANIZATION="$ORGANIZATION" --timeout=10000 &
     pids+=($!)
     echo Dispatched "$MODEL_VERSION" on ROS "$ROS_VERSION"
   done
